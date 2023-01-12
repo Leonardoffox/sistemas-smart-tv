037dbbfa53552de19e2306026bfb683f1dd0a4cb
Leonardo <leo.ancora2@gmail.com>
2023-01-12 16:58:00 -0300

    first commit

---
+++ src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv  smartTv = new SmartTv();
+
+        System.out.println("tV ligada ?:" + smartTv.ligada);
+        System.out.println("Canal Atual:" + smartTv.canal);
+        System.out.println("Volume Atual:"  + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV ligada ?:" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada ?:" + smartTv.ligada);
+    }
+}
